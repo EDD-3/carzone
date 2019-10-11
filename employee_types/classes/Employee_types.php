a1a7530a8651c7aea6c62fc7ad63da28d5d18018
TamalDeCarne <TamalDeCarne@users.noreply.github.com>
2019-10-10 18:44:36 -0700

    Valer verga
    Estar sad

---
+++ employee_types/classes/Employee_types.php
@@ -0,0 +1,138 @@
+<?php
+
+require_once('../config/database.php');
+
+/*
+    Clase que hereda la conexion a base de datos
+*/
+class Employee extends Connection
+{
+
+    /*
+    Funcion que recibe un array de datos para su inserción
+    */
+    function insert($data)
+    {
+        $return_value = [
+            'data'=>false,
+            'error'=>false,
+            'response'=>''
+        ];
+        $cnx = $this->connect();
+
+        try
+        {
+            $statement = $cnx->prepare("INSERT INTO employee_types (title:title)");
+            $statement->execute($data);
+        }
+    
+        catch(PDOException $e)
+        {
+            $return_value['error'] = true;
+            $return_value['response']=$e->getMessage();
+        }
+        return $return_value;
+    }
+
+    function read()
+    {
+        $return_value = [
+            'data'=>false,
+            'error'=>false,
+            'response'=>''
+        ];
+
+        $cnx = $this->connect();
+
+        try
+        {
+            $statement = $cnx->prepare("SELECT * FROM employee_types");
+            $statement->execute();
+            $return_value['response'] = [];
+            while($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {     
+                array_push($return_value['response'],$row);
+            }
+        }
+        catch(PDOException $e)
+        {
+            $return_value['error'] = true;
+            $return_value['response']=$e->getMessage();
+        }
+        return $return_value;
+    }
+
+    function show($data)
+    {
+        $return_value = [
+            'data'=>false,
+            'error'=>false,
+            'response'=>''
+        ];
+
+        $cnx = $this->connect();
+
+        try
+        {
+            $statement = $cnx->prepare("SELECT * FROM employee_types WHERE id = :id");
+            $statement->execute($data);
+            $return_value['response'] = [];
+            while($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                array_push($return_value['response'],$row);
+            }
+           
+        }
+        catch(PDOException $e)
+        {
+            $return_value['error'] = true;
+            $return_value['response']=$e->getMessage();
+        }
+        return $return_value;
+    }
+
+    function update($data)
+    {
+        $return_value = [
+            'data'=>false,
+            'error'=>false,
+            'response'=>''
+        ];
+        $cnx = $this->connect();
+
+        try
+        {
+            $statement = $cnx->prepare("UPDATE employee_types SET title=:title WHERE id = :id");
+            $statement->execute($data);
+        }
+        catch(PDOException $e)
+        {
+            $return_value['error'] = true;
+            $return_value['response']=$e->getMessage();
+        }
+        return $return_value;
+    }
+
+    function delete($data)
+    {
+        $return_value = [
+            'data'=>false,
+            'error'=>false,
+            'response'=>''
+        ];
+
+        $cnx = $this->connect();
+
+        try
+        {
+            $statement = $cnx->prepare("DELETE FROM employee_types WHERE id = :id");
+            $statement->execute($data);
+        }
+        catch(PDOException $e)
+        {
+            $return_value['error'] = true;
+            $return_value['response']=$e->getMessage();
+        }
+        return $return_value;
+    }
+}
